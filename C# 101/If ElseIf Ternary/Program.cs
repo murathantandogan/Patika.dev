39f9a983d8ed05aa805a0dea2e9d3aa5199b8e39
Murathan Tandoğan <murathantandogan@hotmail.com>
2022-03-05 19:30:03 +0300

    C# 101
    If ElseIf Ternary

---
+++ C# 101/If ElseIf Ternary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace If_ElseIf_Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int time = DateTime.Now.Hour;
+            if (time>=6 && time < 11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (time <=10)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+
+            string sonuc = time <= 18 ? "İyi GÜnler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
