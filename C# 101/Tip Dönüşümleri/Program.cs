631d8b4befb878468a45ce33e91782357761cf35
Murathan Tandoğan <murathantandogan@hotmail.com>
2022-03-05 18:51:51 +0300

    C# 101
    Tip Dönüşümleri

---
+++ C# 101/Tip Dönüşümleri/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tip_Dönüşümleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a; int b = 25; a = b;           
+
+            short x = 10; int y; y = x;
+
+
+            Console.WriteLine(a);
+            Console.WriteLine(x);
+        }
+    }
+}
